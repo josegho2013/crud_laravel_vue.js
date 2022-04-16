5370b7b74e23840e87335250ceb91295b26d52b2
jose hernandez <josegho2013@gmail.com>
2022-04-15 21:57:43 -0300

    update

--- crud-laravel-vue/resources/js/app.js
+++ crud-laravel-vue/resources/js/app.js
@@ -1,1 +1,55 @@
-require('./bootstrap');
+// require("./bootstrap");
+// import vue from "vue";
+// window.Vue = vue;
+
+// import App from "./components/App.vue";
+
+// //importamos Axios
+// import VueAxios from "vue-axios";
+// import axios from "axios";
+
+// //Importamos y configuramos el Vue-router
+// import VueRouter from "vue-router";
+// import { routes } from "./routes";
+
+// Vue.use(VueRouter);
+// Vue.use(VueAxios, axios);
+
+// const router = new VueRouter({
+//     mode: "history",
+//     routes: routes,
+// });
+
+// //finalmente, definimos nuestra app de Vue
+// const app = new Vue({
+//     el: "#app",
+//     router: router,
+//     render: (h) => h(App),
+// });
+
+
+
+require("./bootstrap");
+import { createApp } from 'vue'
+import * as VueRouter from "vue-router";
+import VueAxios from "vue-axios";
+import axios from 'axios'
+import { routes } from "./routes";
+import App from "./components/App.vue";
+
+const router = new VueRouter({
+    mode: "history",
+    routes: routes,
+});
+
+const app = createApp(App)
+app.use(VueAxios, axios)
+app.use(router)
+app.mount('#app')
+
+//finalmente, definimos nuestra app de Vue
+// const app = new Vue({
+//     el: "#app",
+//     router: router,
+//     render: (h) => h(App),
+// });
--- crud-laravel-vue/routes/api.php
+++ crud-laravel-vue/routes/api.php
@@ -3,17 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//de esta forma SOLO nos genera las rutas de los metodos que estan en el only
+//Route::resource('blog',App\Http\Controllers\BlogController::class)->only(['index','store','show','update','destroy']);
+
+//de esta forma nos genera todas las rutas
+Route::resource('blog',App\Http\Controllers\BlogController::class);
